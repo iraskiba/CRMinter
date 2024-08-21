3a9ae765ce62a9a9061153907063fcffd0a0986b
iraskiba <bruxoveczkayai@gmail.com>
2024-08-21 10:04:42 +0300

    added select and styles
    added select and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
 import { Button } from 'antd'
+import UniversalSelect from './shared/ui/form-items/select/UniversalSelect.tsx'
+import React from 'react'
+import { FormProvider, useForm } from 'react-hook-form'
 
 function App() {
+  const methods = useForm({
+    defaultValues: {
+      test: 'option1',
+    },
+  })
+
   return (
     <div className="App">
-      <Button type="primary">Button</Button>
-      <Button type="default">Button</Button>
+      <FormProvider {...methods}>
+        <UniversalSelect name={'test'} />
+        <Button type="primary">Button</Button>
+        <Button type="default">Button</Button>
+        <Test>f</Test>
+      </FormProvider>
     </div>
   )
 }
-
+function Test({
+  children,
+  ...props
+}: React.DetailedHTMLProps<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+> &
+  React.PropsWithChildren) {
+  return <button {...props}>{children}</button>
+}
 export default App
---
+++ src/shared/ui/form-items/select/UniversalSelect.tsx
@@ -0,0 +1,32 @@
+import { useController, useFormContext } from 'react-hook-form'
+import { Select } from 'antd'
+import { options } from './options.ts'
+import { SelectProps } from 'antd/es/select'
+import styles from './styles.module.css'
+
+const { Option } = Select
+type Props = { name: string } & SelectProps //кастомные пропсы + наследование пропсов из библиотеки
+const UniversalSelect = ({ name, ...selectProps }: Props) => {
+  const { control } = useFormContext()
+  const {
+    field,
+    fieldState: { error },
+  } = useController({
+    name,
+    control,
+  })
+
+  return (
+    <div className={styles.select1}>
+      <Select {...field} {...selectProps}>
+        {options.map((option) => (
+          <Option key={option.value} value={option.value}>
+            {option.label}
+          </Option>
+        ))}
+      </Select>
+      {error && <p>{error.message}</p>}
+    </div>
+  )
+}
+export default UniversalSelect
