1dc04290a4caf440e36fe392c5498af8cc32b1b8
iraskiba <bruxoveczkayai@gmail.com>
2024-09-17 12:14:25 +0300

    removed use-sync-external-store,react-use; installed auto-zustand-selectors-hook, changed saving of post query result

--- src/pages/tasks/model/api.tsx
+++ src/pages/tasks/model/api.tsx
@@ -6,6 +6,6 @@ type TaskFormValues = {
   description: string
 }
 export const postTask = async (task: TaskFormValues) => {
-  const response = await axios.post('http://localhost:3001/tasks', task)
-  return response.data
+  const { data } = await axios.post('http://localhost:3001/tasks', task)
+  return data
 }
